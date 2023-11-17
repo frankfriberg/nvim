c2dcd3a76937284775d8a1d47ceb6287046b8811
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2023-11-17 07:03:58 +0100

    feat(plugin): change what plugin to use for pairs

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,5 +1,21 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  config = true
+  'altermo/ultimate-autopair.nvim',
+  event = { 'InsertEnter', 'CmdlineEnter' },
+  opts = {
+    tabout = {
+      enable = true
+    },
+    fastwarp = {
+      enable = true,
+      map = "<C-e>",
+      rmap = "<C-E>",
+      cmap = "<C-e>",
+      rcmap = "<C-E>",
+    },
+    close = {
+      enable = true,
+      map = "<A-e>",
+      cmap = "<A-e>"
+    }
+  }
 }
