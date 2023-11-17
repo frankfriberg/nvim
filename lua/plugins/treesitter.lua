3dc85a0971fd26b29adb692fc5028094fef548c6
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2023-11-17 07:06:49 +0100

    chore(plugin): remove unused code from treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
@@ -5,40 +6,22 @@ return {
     "windwp/nvim-ts-autotag"
   },
   build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
   config = function()
     require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "typescript", "javascript", "lua"
+      },
       highlight = {
         enable = true
       },
       indent = {
         enable = true
       },
-      autotag = {
-        enable = true,
-      },
       auto_install = true,
       context_commentstring = {
         enable = true,
         enable_autocmd = false,
       },
-      ensure_installed = {
-        "bash",
-        "help",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-      }
     })
   end,
 }
