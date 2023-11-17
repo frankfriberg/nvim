ad49d099a872e3b12ebf18e0db21cf408e488ead
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2023-11-17 06:57:31 +0100

    feat(ui): add more custom highlights

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -6,10 +6,14 @@ return {
   config = function()
     require("everforest").setup({
       transparent_background_level = 2,
-      on_highlights = function (hl, palette)
+      on_highlights = function(hl)
         hl.NeoTreeDirectoryIcon = { link = "Green" }
-        hl.TelescopeBorder = { link = "Fg"}
+        hl.NeoTreeFloatBorder = { link = "FloatBorder" }
         hl.VertSplit = { link = "Fg" }
+        hl.NormalFloat = { link = "Normal" }
+        hl.FloatBorder = { link = "Normal" }
+        hl.FloatTitle = { link = "Normal" }
+        hl.TelescopeBorder = { link = "FloatBorder" }
       end
     })
   end
