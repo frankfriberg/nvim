e9d93d7aa8b7ebe688bc4442b04be961068afa2b
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2023-11-17 06:56:34 +0100

    refactor(plugin): change what comment plugin is used

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,14 @@
 return {
-  "numToStr/Comment.nvim",
+  'echasnovski/mini.comment',
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
   event = "VeryLazy",
-  config = true,
+  opts = {
+    options = {
+      custom_commentstring = function()
+        return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
+      end,
+    }
+  }
 }
