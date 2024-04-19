19012e3388efd03d858e9e1e95df996c4248aeed
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-19 23:05:17 +0200

    feat: add telescope

--- lua/plugins/lsp/on-attach.lua
+++ lua/plugins/lsp/on-attach.lua
@@ -3,12 +3,17 @@ return function(event)
     vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
   end
 
-  -- map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  -- map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  -- map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  -- map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  -- map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  -- map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  local telescope_ok, builtin = pcall(require, "telescope.builtin")
+
+  if telescope_ok then
+    map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+    map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+    map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+    map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+    map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+    map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+  end
+
   map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
   map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
   map("K", vim.lsp.buf.hover, "Hover Documentation")
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,67 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    },
+  },
+  config = function()
+    local m = require("ff.map")
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions")
+
+    local function get_selected_text()
+      local a_orig = vim.fn.getreg("a")
+      local mode = vim.fn.mode()
+      if mode ~= "v" and mode ~= "V" then
+        vim.cmd([[normal! gv]])
+      end
+      vim.cmd([[normal! "aygv]])
+      local text = vim.fn.getreg("a")
+      vim.fn.setreg("a", a_orig)
+      return text
+    end
+
+    m.t({
+      c_f = { builtin.find_files, "[T] Find files" },
+      c_g = { builtin.live_grep, "[T] Live grep" },
+      c_p = { builtin.resume, "[T] Resume" },
+      c_c = { builtin.grep_string, "[T] Grep cursor" },
+      c_q = { builtin.quickfix, "[T] Quickfix" },
+      {
+        mode = "v",
+        c_c = {
+          function()
+            local text = get_selected_text()
+            builtin.grep_string({ search = text })
+          end,
+          "[T] Grep cursor",
+        },
+      },
+    })
+
+    telescope.setup({
+      defaults = {
+        file_ignore_patterns = { "vendor" },
+        selection_caret = "",
+        entry_prefix = "",
+        prompt_prefix = "",
+        dynamic_preview_title = true,
+        mappings = {
+          i = {
+            ["<Tab>"] = "move_selection_worse",
+            ["<S-Tab>"] = "move_selection_better",
+            ["<C-k>"] = actions.cycle_history_next,
+            ["<C-j>"] = actions.cycle_history_prev,
+            ["<C-a>"] = actions.toggle_selection,
+            ["<esc>"] = actions.close,
+          },
+        },
+        layout_strategy = "flex",
+      },
+    })
+  end,
+}
