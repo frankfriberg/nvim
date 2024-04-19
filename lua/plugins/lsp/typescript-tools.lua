2003bd1eaac0bf5ee95e401d9724b3c0abc44960
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-19 14:45:59 +0200

    chore: move typescript to lsp init

--- lua/plugins/lsp/typescript-tools.lua
+++ lua/plugins/lsp/typescript-tools.lua
@@ -2,6 +2,7 @@ return {
   "pmizio/typescript-tools.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "neovim/nvim-lspconfig"
   },
   config = function()
     local m = require("ff.map")
