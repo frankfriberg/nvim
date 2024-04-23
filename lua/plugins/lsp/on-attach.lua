cc287dd2cab2191a3481b098da54d013954cf1f4
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-23 08:05:27 +0200

    refactor: use mapper to map on-attach bindings

--- lua/plugins/lsp/on-attach.lua
+++ lua/plugins/lsp/on-attach.lua
@@ -1,23 +1,29 @@
 return function(event)
-  local map = function(keys, func, desc)
-    vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-  end
+  local map = require("helpers.map")
 
   local telescope_ok, builtin = pcall(require, "telescope.builtin")
 
   if telescope_ok then
-    map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
-    map("gr", builtin.lsp_references, "[G]oto [R]eferences")
-    map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
-    map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
-    map("<leader>ds", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
-    map("<leader>ws", builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    map.t({
+      gd = { builtin.lsp_definitions, "[G]oto [D]efinition" },
+      gr = { builtin.lsp_references, "[G]oto [R]eferences" },
+      gI = { builtin.lsp_implementations, "[G]oto [I]mplementation" },
+      l_D = { builtin.lsp_type_definitions, "Type [D]efinition" },
+      l_ds = { builtin.lsp_document_symbols, "[D]ocument [S]ymbols" },
+      l_ws = { builtin.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols" },
+    })
   end
 
-  map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-  map("K", vim.lsp.buf.hover, "Hover Documentation")
-  map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  map.t({
+    K = { vim.lsp.buf.hover, "Hover Documentation" },
+    gD = { vim.lsp.buf.declaration, "[G]oto [D]eclaration" },
+    {
+      group = { "<leader>l", "LSP" },
+      k = { vim.lsp.buf.signature_help, "Signature Help" },
+      r = { vim.lsp.buf.rename, "[R]e[n]ame" },
+      c = { vim.lsp.buf.code_action, "[C]ode Action" },
+    },
+  })
 
   local client = vim.lsp.get_client_by_id(event.data.client_id)
   if client and client.server_capabilities.documentHighlightProvider then
@@ -33,7 +39,7 @@ return function(event)
   end
 
   if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-    map("<leader>th", function()
+    map.n("<leader>lt", function()
       vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
     end, "[T]oggle Inlay [H]ints")
   end
