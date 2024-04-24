48df357418bde607ab84deb1de6f34f969356e28
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-24 22:09:34 +0200

    feat: add border to mason ui

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -34,7 +34,11 @@ return {
         jsonls = require("plugins.lsp.servers.jsonls"),
       }
 
-      require("mason").setup()
+      require("mason").setup({
+        ui = {
+          border = "rounded",
+        }
+      })
 
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
