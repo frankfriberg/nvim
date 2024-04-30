afa04fe74a9cf7aec152035526c29a6d46045664
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-30 08:44:52 +0200

    fix: update for 0.10

--- lua/plugins/lsp/on-attach.lua
+++ lua/plugins/lsp/on-attach.lua
@@ -40,7 +40,7 @@ return function(event)
 
   if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
     map.n("<leader>lt", function()
-      vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
     end, "[T]oggle Inlay [H]ints")
   end
 end
