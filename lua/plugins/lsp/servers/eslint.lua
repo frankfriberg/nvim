b8131f4586f884e413fe808a431dd883e9665320
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-30 08:45:03 +0200

    feat: add auto fix for eslint

--- lua/plugins/lsp/servers/eslint.lua
+++ lua/plugins/lsp/servers/eslint.lua
@@ -1,4 +1,10 @@
 return {
+  on_attach = function(_, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
   settings = {
     format = {
       enable = false,
