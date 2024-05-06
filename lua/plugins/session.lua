829d6278936c11026a806bc31be3e542462c5f05
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-05-06 14:48:45 +0200

    refactor: use resession for session management
    This is because resession has way better control over behaviour. This is
    self managed solution instead of understanding the inner workings of a
    plugin

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,81 +1,90 @@
 return {
-  "Shatur/neovim-session-manager",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  lazy = false,
-  priority = 900,
-  init = function() end,
-  opts = function()
-    local sm_config = require("session_manager.config")
-    local sm_utils = require("session_manager.utils")
-    local Path = require("plenary.path")
-    local path_replacer = "__"
-    local colon_replacer = "++"
+  "stevearc/resession.nvim",
+  config = function()
+    local autocmd = vim.api.nvim_create_autocmd
+    local resession = require("resession")
 
-    local function get_branch_name()
-      if vim.g.branch_name then
-        return vim.g.branch_name
-      end
+    resession.setup({
+      buf_filter = function(bufnr)
+        local buftype = vim.bo[bufnr].buftype
+        if buftype == "help" then
+          return true
+        end
 
-      local branch = vim.fn.system("git branch --show-current")
+        if buftype ~= "" and buftype ~= "acwrite" then
+          return false
+        end
 
-      if branch ~= "" then
-        return branch:gsub("\n", "")
+        if buftype == "nofile" then
+          return false
+        end
+
+        if vim.api.nvim_buf_get_name(bufnr) == "" then
+          return false
+        end
+
+        return vim.bo[bufnr].buflisted
+      end,
+    })
+
+    local function get_session_name()
+      local name = vim.fn.getcwd()
+      local branch = vim.trim(vim.fn.system("git branch --show-current"))
+      if vim.v.shell_error == 0 then
+        return name .. branch
       else
-        return nil
+        return name
       end
     end
 
-    local function convert_filename_to_dir(filename)
-      local filename_without_extra = filename:sub(1, (filename:find("@") or 0) - 1)
-      local dir = filename_without_extra:sub(#tostring(sm_config.sessions_dir) + 2)
-
-      dir = dir:gsub(colon_replacer, ":")
-      dir = dir:gsub(path_replacer, Path.path.sep)
-      return Path:new(dir)
-    end
-
-    local function convert_dir_to_filename(dir, branch)
-      if not branch then
-        branch = get_branch_name()
-      end
-
-      local filename = dir:gsub(":", colon_replacer)
-      filename = filename:gsub(Path.path.sep, path_replacer)
-
-      if branch then
-        filename = filename .. "@" .. branch
-      end
-
-      return Path:new(sm_config.sessions_dir):joinpath(filename)
-    end
-
-    vim.api.nvim_create_autocmd({ "User" }, {
-      pattern = "NeogitBranchCheckout",
-      callback = function(ctx)
-        local cwd = vim.loop.cwd()
-
-        if cwd then
-          local session = convert_dir_to_filename(cwd, ctx.data.branch_name)
-          if session:exists() then
-            sm_utils.load_session(session.filename)
-          end
+    autocmd("VimEnter", {
+      callback = function()
+        -- Only load the session if nvim was started with no args
+        if vim.fn.argc(-1) == 0 then
+          resession.load(get_session_name(), { dir = "dirsession", silence_errors = true })
         end
       end,
     })
 
-    return {
-      autoload_mode = sm_config.AutoloadMode.CurrentDir,
-      session_filename_to_dir = convert_filename_to_dir,
-      dir_to_session_filename = convert_dir_to_filename,
-      autosave_last_session = true,
-      autosave_ignore_not_normal = true,
-      autosave_ignore_filetypes = {
-        "gitcommit",
-        "gitrebase",
-      },
-      max_path_length = 0,
-    }
+    autocmd("VimLeavePre", {
+      callback = function()
+        resession.save(get_session_name(), { dir = "dirsession", notify = false })
+      end,
+    })
+
+    autocmd("User", {
+      pattern = "NeogitStatusRefreshed",
+      callback = function()
+        resession.save(get_session_name(), { dir = "dirsession", notify = false })
+      end,
+    })
+
+    autocmd({ "User" }, {
+      pattern = "NeogitBranchCheckout",
+      callback = function(ctx)
+        require("neogit").close()
+
+        local name = vim.fn.getcwd()
+        local session_name = name .. ctx.data.branch_name
+        local session_file = require("resession.util").get_session_file(session_name, "dirsession")
+        local session_data = require("resession.files").load_json_file(session_file)
+
+        if session_data then
+          resession.load(session_name, { dir = "dirsession", silence_errors = true })
+        else
+          local scratch = vim.api.nvim_create_buf(false, true)
+          vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = scratch })
+          vim.api.nvim_win_set_buf(0, scratch)
+          vim.api.nvim_set_option_value("buftype", "", { buf = scratch })
+          for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.bo[bufnr].buflisted then
+              vim.api.nvim_buf_delete(bufnr, { force = true })
+            end
+          end
+
+          resession.save(session_name, { dir = "dirsession", notify = false })
+        end
+      end,
+    })
   end,
 }
