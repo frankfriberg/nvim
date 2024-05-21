04bdb6322a108dac99ec7d7fb40505e6e94fb9b6
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-05-21 10:23:13 +0200

    refactor: add just ts support for comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,21 +1,6 @@
 return {
-  "echasnovski/mini.comment",
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  opts = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("ts_context_commentstring").setup({
-      enable_autocmd = false,
-    })
-
-    return {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
-        end,
-        ignore_blank_line = true,
-      },
-    }
-  end,
+  "folke/ts-comments.nvim",
+  event = "VeryLazy",
+  config = true,
+  enabled = vim.fn.has("nvim-0.10.0") == 1,
 }
