53b5297cb97e8668a90c767bb8dbecbfd52327d2
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-05-22 07:08:03 +0200

    refactor: update autocommands for neogit

--- lua/plugins/git/neogit.lua
+++ lua/plugins/git/neogit.lua
@@ -17,21 +17,8 @@ return {
       },
     })
 
-    vim.api.nvim_create_autocmd("WinClosed", {
-      callback = function()
-        local current_win = vim.fn.expand("<afile>") + 0
-        local current_buf = vim.api.nvim_win_get_buf(current_win)
-        local filetype = vim.api.nvim_get_option_value("filetype", { buf = current_buf })
-        local neogit_filetype = string.match(filetype, "^Neogit")
-
-        if neogit_filetype and not filetype ~= "NeogitStatus" and neogit.status.is_open() then
-          neogit.status:focus()
-        end
-      end,
-    })
-
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = "NeogitCommitMessage",
+      pattern = { "NeogitCommitMessage", "NeogitRebaseTodo" },
       callback = function()
         map.n("<CR>", ":wq <CR>", "Finish commit")
       end,
