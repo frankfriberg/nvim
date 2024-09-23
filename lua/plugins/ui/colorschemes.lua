e15ca7200282187333cb1468511f552cc561a71b
Frank Friberg <frankfriberg@users.noreply.github.com>
2024-09-23 09:56:08 +0200

    style: update colors of ui

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -17,10 +17,28 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      local float_bg = "surface"
       require("rose-pine").setup({
         dark_variant = "moon",
+        enable = {
+          legacy_highlights = false,
+          migrations = false,
+        },
         highlight_groups = {
+          NormalFloat = { bg = float_bg },
+          FloatBorder = { fg = "text" },
+          Cursorline = { bg = "pine", blend = 10 },
           StatusLine = { bg = "none" },
+          StatusLineNC = { bg = "none" },
+          StatusLineTerm = { bg = "none" },
+          StatusLineTermNC = { bg = "none" },
+          TelescopePromptBorder = { fg = "foam", bg = float_bg },
+          TelescopePromptTitle = { fg = "foam", bg = float_bg },
+          TelescopeResultsBorder = { fg = "iris", bg = float_bg },
+          TelescopeResultsTitle = { fg = "iris", bg = float_bg },
+          TelescopePreviewTitle = { fg = "rose", bg = float_bg },
+          TelescopePreviewBorder = { fg = "rose", bg = float_bg },
+          Pmenu = { bg = float_bg },
         },
       })
 
