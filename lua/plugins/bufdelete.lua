60a7e879cc7e39510749990b6431021e50f3f4bd
Frank Friberg <frankfriberg@users.noreply.github.com>
2024-12-03 18:48:38 +0100

    chore: use `snacks.nvim` bufdelete as it is more flexible

--- lua/plugins/bufdelete.lua
+++ lua/plugins/bufdelete.lua
@@ -1,24 +1,40 @@
 return {
-  "echasnovski/mini.bufremove",
+
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
   keys = {
     {
-      "<leader>q",
+      "<leader>qq",
       function()
-        local bd = require("mini.bufremove").delete
+        local bd = require("snacks").bufdelete.delete
         if vim.bo.modified then
           local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
           if choice == 1 then -- Yes
             vim.cmd.write()
-            bd(0)
+            bd()
           elseif choice == 2 then -- No
-            bd(0, true)
+            bd({ 0, true })
           end
         else
-          bd(0)
+          bd()
         end
       end,
       desc = "Delete Buffer",
     },
+    {
+      "<leader>qa",
+      function()
+        require("snacks").bufdelete.all()
+      end,
+      desc = "Delete All Buffers",
+    },
+    {
+      "<leader>qo",
+      function()
+        require("snacks").bufdelete.other()
+      end,
+      desc = "Delete Other Buffers",
+    },
   },
-  config = true,
 }
