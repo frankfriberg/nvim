9b932fd8f0b5159555e5a2da0671acc2772d4f56
Frank Friberg <frankfriberg@users.noreply.github.com>
2024-12-03 18:49:11 +0100

    fix: show only diagnostic when not in floating

--- lua/ff/autocmd.lua
+++ lua/ff/autocmd.lua
@@ -87,6 +87,11 @@ autocmd({
 autocmd("CursorHold", {
   desc = "Show diagnostics under cursor",
   callback = function()
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+      if vim.api.nvim_win_get_config(winid).zindex then
+        return
+      end
+    end
     vim.diagnostic.open_float()
   end,
 })
