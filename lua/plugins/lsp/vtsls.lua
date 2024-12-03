a3e5087885931bec594d9819dab4e550e929c374
Frank Friberg <frankfriberg@users.noreply.github.com>
2024-12-03 18:50:26 +0100

    chore: use `vtsls` for typescript instead of `typescript-tools`

---
+++ lua/plugins/lsp/vtsls.lua
@@ -0,0 +1,17 @@
+return {
+  "yioneko/nvim-vtsls",
+  config = function()
+    local map = require("helpers.map")
+    local vtsls = require("vtsls")
+
+    map.t({
+      group = { "<leader>t", "TypeScript" },
+      r = { vtsls.commands.remove_unused, "[TS] Remove Unused" },
+      R = { vtsls.commands.remove_unused_imports, "[TS] Remove Unused Imports" },
+      o = { vtsls.commands.organize_imports, "[TS] Organize Imports" },
+      s = { vtsls.commands.sort_imports, "[TS] Sort Imports" },
+      a = { vtsls.commands.add_missing_imports, "[TS] Add Missing Imports" },
+      x = { vtsls.commands.fix_all, "[TS] Fix All" },
+    })
+  end,
+}
