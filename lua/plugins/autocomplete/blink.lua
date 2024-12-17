2cebede87db60c55f9498c5e68c68d3877ca9719
Frank Friberg <frankfriberg@users.noreply.github.com>
2024-12-17 09:33:26 +0100

    refactor: update `blink` config for new version

--- lua/plugins/autocomplete/blink.lua
+++ lua/plugins/autocomplete/blink.lua
@@ -8,35 +8,12 @@ return {
   ---@type blink.cmp.Config
   opts = {
     keymap = {
-      preset = "default",
       ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
       ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       ["<S-CR>"] = { "accept" },
     },
-    nerd_font_variant = "mono",
-    highlight = {
-      use_nvim_cmp_as_default = true,
-    },
-    sources = {
-      completion = {
-        enabled_providers = { "lsp", "path", "buffer" },
-      },
-      providers = {
-        path = {
-          name = "Path",
-          module = "blink.cmp.sources.path",
-          score_offset = 3,
-          opts = {
-            trailing_slash = false,
-            label_trailing_slash = true,
-            get_cwd = function(context)
-              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-            end,
-            show_hidden_files_by_default = false,
-            fallback_for = { "lsp" },
-          },
-        },
-      },
+    appearance = {
+      nerd_font_variant = "mono",
     },
     completion = {
       documentation = {
@@ -47,29 +24,17 @@ return {
           winblend = vim.o.winblend,
         },
       },
-      signature = {
-        enabled = true,
-        window = {
-          border = vim.g.border,
-          winblend = vim.o.winblend,
-        },
-      },
       menu = {
-        border = vim.g.border,
         min_width = 30,
-        columns = { { "kind_icon" }, { "label", "label_description" }, { "label_detail" } },
-        padding = 0,
-        draw = {
-          components = {
-            label_detail = {
-              width = { max = 30 },
-              text = function(ctx)
-                return ctx.label_detail
-              end,
-              highlight = "Comment",
-            },
-          },
-        },
+        border = vim.g.border,
+        winblend = vim.o.winblend,
+      },
+    },
+    signature = {
+      enabled = true,
+      window = {
+        border = vim.g.border,
+        winblend = vim.o.winblend,
       },
     },
   },
