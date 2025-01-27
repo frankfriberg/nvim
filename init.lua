0d77ab7917d44603586bfe770b1fdf9ce1f0f78f
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-01-27 07:09:20 +0100

    style: add borders to `lazy` window

--- init.lua
+++ init.lua
@@ -16,4 +16,18 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  ui = {
+    border = vim.g.border,
+    backdrop = 100,
+    title = " Lazy ",
+    pills = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
