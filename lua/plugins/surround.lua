8e857fdfdaaeb4f2e65763f49b0a1e119c918d55
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-03-12 07:12:50 +0100

    chore: add more surround plugins

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,7 +1,29 @@
 return {
-  "NStefan002/visual-surround.nvim",
-  event = "VeryLazy",
-  opts = {
-    enable_wrapped_deletion = true,
+  {
+    "NStefan002/visual-surround.nvim",
+    event = "VeryLazy",
+    opts = {
+      enable_wrapped_deletion = true,
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "s",
+        normal_cur = "ss",
+        normal_line = "S",
+        normal_cur_line = "SS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "sd",
+        change = "sd",
+        change_line = "Sc",
+      },
+    },
   },
 }
