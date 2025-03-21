fd396effc8b96cb036804fe106e9bdf62171cb63
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-03-21 14:15:08 +0100

    feat: add check to see if biome is in root for prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,17 +1,29 @@
+local js_formatters = { "prettierd", "biome-check" }
+
 return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      javascript = { "prettierd" },
-      typescript = { "prettierd" },
-      javascriptreact = { "prettierd" },
-      typescriptreact = { "prettierd" },
-      json = { "fixjson", "prettierd" },
+      javascript = js_formatters,
+      typescript = js_formatters,
+      javascriptreact = js_formatters,
+      typescriptreact = js_formatters,
+      json = { "prettierd", "biome-check", "jsonfix" },
       lua = { "stylua" },
     },
     format_on_save = {
       timeout_ms = 500,
       lsp_fallback = true,
+    },
+    formatters = {
+      ["biome-check"] = {
+        require_cwd = true,
+      },
+      prettierd = {
+        condition = function(_, ctx)
+          return vim.fs.find("biome.json", { upward = true, path = ctx.dirname })[1] == nil
+        end,
+      },
     },
   },
 }
