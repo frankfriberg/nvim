7af6eb1fbb0cdb50e785a257cf948a47e1dbf228
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-03-21 14:13:17 +0100

    chore: remove unecessary tailwind check

--- lua/plugins/lsp/servers/tailwindcss.lua
+++ lua/plugins/lsp/servers/tailwindcss.lua
@@ -1,7 +1,4 @@
-local root_pattern = require("lspconfig.util").root_pattern
-
 return {
-  root_dir = root_pattern("tailwind.config.js", "tailwind.config.cjs", "tailwind.config.mjs", "tailwind.config.ts"),
   settings = {
     tailwindCSS = {
       experimental = {
