f29a36f392a325592051eb00742f9b86909f1783
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-04-03 11:39:47 +0200

    style: reset whichkey custom style

--- lua/plugins/ui/whichkey.lua
+++ lua/plugins/ui/whichkey.lua
@@ -2,21 +2,6 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    win = {
-      width = { min = 30, max = 60 },
-      height = { min = 4, max = 0.75 },
-      padding = { 0, 1 },
-      row = 0,
-      col = 0.5,
-      border = "rounded",
-      title = true,
-      title_pos = "left",
-      wo = {
-        winblend = vim.o.winblend,
-      },
-    },
-    layout = {
-      width = { min = 30 },
-    },
+    preset = "modern",
   },
 }
