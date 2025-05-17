03b3acf9688d12491ec64fad7f77688e465f6151
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-05-17 07:24:22 +0200

    feat: add legendary.nvim

---
+++ lua/plugins/legendary.lua
@@ -0,0 +1,20 @@
+return {
+  "mrjones2014/legendary.nvim",
+  priority = 10000,
+  lazy = false,
+  opts = {
+    extensions = {
+      lazy_nvim = true,
+      which_key = {
+        auto_register = true,
+      },
+    },
+    col_separator_char = "-",
+    icons = {
+      keymap = nil,
+      command = "$",
+      fn = "󰊕",
+      itemgroup = "󰅩",
+    },
+  },
+}
