06259817f4cadd3f17f1b9ba5083b2430399d597
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-06-26 10:48:29 +0200

    refactor: add back custom lsp keymaps

--- lua/helpers/on-attach.lua
+++ lua/helpers/on-attach.lua
@@ -1,7 +1,18 @@
 return function(event)
   local map = require("helpers.map")
 
-  vim.keymap.set("n", "gd", "<C-]>", { desc = "Go to definition" })
+  map.t({
+    gD = { vim.lsp.buf.declaration, "[G]oto [D]eclaration", false },
+    gd = { "<C-]>", "Goto Definition", false },
+    gr = { vim.lsp.buf.references, "Goto References", false },
+    gI = { vim.lsp.buf.implementation, "Goto Implementations", false },
+    {
+      group = { "<leader>l", "LSP" },
+      k = { vim.lsp.buf.signature_help, "Signature Help", false },
+      r = { vim.lsp.buf.rename, "[R]e[n]ame", false },
+      c = { vim.lsp.buf.code_action, "[C]ode Action", false },
+    },
+  })
 
   local client = vim.lsp.get_client_by_id(event.data.client_id)
   if not client then
