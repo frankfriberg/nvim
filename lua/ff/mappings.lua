6c3acc5cf6920cdf7a8f8db7c40ba8cb4fe2dd1d
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-07-30 10:33:24 +0200

    feat: add move keymaps for terminal

--- lua/ff/mappings.lua
+++ lua/ff/mappings.lua
@@ -31,6 +31,13 @@ map.t({
   },
   c_j = { "<C-n>", "Next menu item" },
   c_k = { "<C-p>", "Previous menu item" },
+  {
+    mode = "t",
+    c_h = { "<C-\\><C-n><C-w>h", "Move to left window from terminal" },
+    c_j = { "<C-\\><C-n><C-w>j", "Move to below window from terminal" },
+    c_k = { "<C-\\><C-n><C-w>k", "Move to above window from terminal" },
+    c_l = { "<C-\\><C-n><C-w>l", "Move to right window from terminal" },
+  },
 })
 
 map.group("n", "<leader>")
