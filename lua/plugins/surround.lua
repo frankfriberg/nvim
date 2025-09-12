c406b91b35af11bcd08672eca445c6de28af64cb
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-09-12 08:52:15 +0200

    chore: remove nvim surround

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -6,24 +6,4 @@ return {
       enable_wrapped_deletion = true,
     },
   },
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    opts = {
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "s",
-        normal_cur = "ss",
-        normal_line = "S",
-        normal_cur_line = "SS",
-        visual = "S",
-        visual_line = "gS",
-        delete = "sd",
-        change = "sd",
-        change_line = "Sc",
-      },
-    },
-  },
 }
