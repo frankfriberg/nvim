69f2d00c324e5309c317182d04404ded98ab5e14
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-09-12 08:51:38 +0200

    feat: add vtsls command for reloading project

--- lua/plugins/lsp/vtsls.lua
+++ lua/plugins/lsp/vtsls.lua
@@ -9,6 +9,8 @@ return {
       group = { "<leader>t", "TypeScript" },
       r = { vtsls.commands.remove_unused, "[TS] Remove Unused" },
       R = { vtsls.commands.remove_unused_imports, "[TS] Remove Unused Imports" },
+      t = { vtsls.commands.restart_tsserver, "[TS] Restart tsserver" },
+      T = { vtsls.commands.reload_projects, "[TS] Reload projects" },
       o = { vtsls.commands.organize_imports, "[TS] Organize Imports" },
       s = { vtsls.commands.sort_imports, "[TS] Sort Imports" },
       a = { vtsls.commands.add_missing_imports, "[TS] Add Missing Imports" },
