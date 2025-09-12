1b70a6e66007fc4170aa4af1edb1f7001a164a77
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-09-12 08:52:50 +0200

    chore: remove some unecessary keybindings

--- lua/ff/mappings.lua
+++ lua/ff/mappings.lua
@@ -2,9 +2,6 @@ local map = require("helpers.map")
 local bufdelete = require("helpers.bufdelete")
 
 map.t({
-  -- Center screen for commands
-  -- c_u = { "<C-u>zz", "Scroll up" },
-  -- c_d = { "<C-d>zz", "Scroll down" },
   l_w = { ":silent! wa <CR>", "Write all buffer" },
   l_q = {
     function()
@@ -30,11 +27,6 @@ map.t({
   {
     mode = "t",
     c_h = { "<C-\\><C-n><C-w>h", "Move to left window from terminal" },
-    c_j = { "<C-\\><C-n><C-w>j", "Move to below window from terminal" },
-    c_k = { "<C-\\><C-n><C-w>k", "Move to above window from terminal" },
     c_l = { "<C-\\><C-n><C-w>l", "Move to right window from terminal" },
   },
 })
-
-map.group("n", "<leader>")
-map.group("n", "g")
