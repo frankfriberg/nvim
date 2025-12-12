8af823abb221cae15fa4c2c08acf8637b9690400
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-12-12 22:55:54 +0100

    refactor: add vitest and update test keymaps

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,65 +4,62 @@ return {
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
-    "nvim-neotest/neotest-jest",
+    -- "nvim-neotest/neotest-jest",
+    "marilari88/neotest-vitest",
     "nvim-treesitter/nvim-treesitter",
   },
-  event = "BufEnter",
+  keys = require("helpers.map").lazy({
+    group = { "<leader>n", "Neotest" },
+    t = {
+      function()
+        return require("neotest").run.run()
+      end,
+      "Run the nearest test",
+    },
+    f = {
+      function()
+        return require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      "Run current file",
+    },
+    a = {
+      function()
+        return require("neotest").run.run(vim.loop.cwd())
+      end,
+      "Run all tests",
+    },
+    w = {
+      function()
+        return require("neotest").run.run({ jestCommand = "jest --watch" })
+      end,
+      "Run test in watch mode",
+    },
+    s = {
+      function()
+        return require("neotest").summary.toggle()
+      end,
+      "Show the summary",
+    },
+    o = {
+      function()
+        return require("neotest").output()
+      end,
+      "Show the output panel",
+    },
+  }),
   config = function()
-    local neotest = require("neotest")
-    local map = require("helpers.map")
-
-    map.t({
-      group = { "<leader>n", "Neotest" },
-      t = {
-        function()
-          return neotest.run.run()
-        end,
-        "[Test] Run the nearest test",
-      },
-      f = {
-        function()
-          return neotest.run.run(vim.fn.expand("%"))
-        end,
-        "[Test] Run current file",
-      },
-      a = {
-        function()
-          return neotest.run.run(vim.loop.cwd())
-        end,
-        "[Test] Run all tests",
-      },
-      w = {
-        function()
-          return neotest.run.run({ jestCommand = "jest --watch" })
-        end,
-        "[Test] Run test in watch mode",
-      },
-      s = {
-        function()
-          return neotest.summary.toggle()
-        end,
-        "[Test] Show the summary",
-      },
-      o = {
-        function()
-          return neotest.output()
-        end,
-        "[Test] Show the output panel",
-      },
-    })
-
-    neotest.setup({
+    require("neotest").setup({
       status = { virtual_text = true },
       output = { open_on_run = true },
       adapters = {
-        require("neotest-jest")({
-          jestCommand = "npm test --",
-          env = { CI = true },
-          cwd = function()
-            return vim.fn.getcwd()
-          end,
-        }),
+        require("neotest-vitest"),
+        -- require("neotest-jest")({
+        --   jestCommand = "npm test --",
+        --   env = { CI = true },
+        --   cwd = function()
+        --     return vim.fn.getcwd()
+        --   end,
+        -- }),
       },
     })
   end,
