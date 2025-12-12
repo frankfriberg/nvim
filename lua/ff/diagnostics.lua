a0e474680ae19706ac15f383d7226e7a281acfb9
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-12-12 22:37:20 +0100

    feat: add more diagnostic keymaps

--- lua/ff/diagnostics.lua
+++ lua/ff/diagnostics.lua
@@ -1,3 +1,5 @@
+local map = require("helpers.map")
+
 -- Diagnostic configuration.
 vim.diagnostic.config({
   float = {
@@ -13,4 +15,20 @@ vim.diagnostic.config({
   severity_sort = true,
   virtual_text = true,
   virtual_lines = false,
+})
+
+local diagnostic_goto = function(next, severity)
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    vim.diagnostic.jump({ severity = severity, count = next and 1 or -1 })
+  end
+end
+
+map.t({
+  ["]d"] = { diagnostic_goto(true), "Next Diagnostic" },
+  ["[d"] = { diagnostic_goto(false), "Prev Diagnostic" },
+  ["]e"] = { diagnostic_goto(true, "ERROR"), "Next Error" },
+  ["[e"] = { diagnostic_goto(false, "ERROR"), "Prev Error" },
+  ["]w"] = { diagnostic_goto(true, "WARN"), "Next Warning" },
+  ["[w"] = { diagnostic_goto(false, "WARN"), "Prev Warning" },
 })
