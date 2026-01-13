0211dffc9947ae25efa8ddf59eefe3a8e78ea0df
Frank Friberg <frankfriberg@users.noreply.github.com>
2026-01-13 20:36:41 +0100

    fix: remove auto dark mode

--- lua/plugins/ui/colorschemes.lua
+++ lua/plugins/ui/colorschemes.lua
@@ -1,17 +1,5 @@
 return {
   {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      update_interval = 500,
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", { scope = "global" })
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", { scope = "global" })
-      end,
-    },
-  },
-  {
     "rose-pine/neovim",
     name = "rose-pine",
     lazy = false,
